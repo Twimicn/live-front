b70ee4c9ad31c2d67dccd2b1a6e00294275fdec2
IvanLuLyf <835498692@qq.com>
2021-03-03 22:48:12 +0800

    add some file

--- js/constant.js
+++ js/constant.js
@@ -2,6 +2,7 @@
     var USER_ROLES = {
         0: '<span class="badge badge-dark">普通用户</span>',
         1: '<span class="badge badge-primary">管理员</span>',
+        2: '<span class="badge badge-info">主播</span>',
     };
     var USER_STATE = {
         0: '<span class="badge badge-secondary">未激活</span>',

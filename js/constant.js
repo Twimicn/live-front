d6bd66548b5a677b14a06b9772834f80cd2ad263
IvanLuLyf <835498692@qq.com>
2021-04-05 00:52:04 +0800

    update constant.js

--- js/constant.js
+++ js/constant.js
@@ -9,8 +9,14 @@
         1: '<span class="badge badge-success">已激活</span>',
         2: '<span class="badge badge-warning">禁用</span>',
     };
+    var LIVE_STATE = {
+        0: '<span class="badge badge-success">直播中</span>',
+        1: '<span class="badge badge-secondary">已结束</span>',
+        2: '<span class="badge badge-warning">隐藏</span>',
+    };
     WIN.Constant = {
         USER_ROLES: USER_ROLES,
         USER_STATE: USER_STATE,
+        LIVE_STATE: LIVE_STATE,
     }
 })(window);
